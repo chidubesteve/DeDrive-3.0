2b84fcf9bff4f060125d8fc77f9d96ef96e5a111
chidubesteve <anikechidube@gmail.com>
2024-03-16 22:13:05 +0100

    Added light mode specific styles for hover state on the buttons

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from "react";
 
 // internal imports
-import style from './Buttons.module.css'
+import style from "./Buttons.module.css";
+import { themeContext } from "../../Theme";
 
 const Buttons = ({ btnName, handleClick }) => {
+  const {theme} = useContext(themeContext);
   return (
     <div className={style.box}>
-      <button className={style.button} onClick={() => handleClick()}>
+      <button
+        className={`${style.button} ${theme ==='light-mode' ? (style.lightMode) : ""}`}
+        onClick={() => handleClick()}
+      >
         {btnName}
       </button>
     </div>
   );
 };
 
-export default Buttons
+export default Buttons;
