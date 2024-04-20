69ecf1c50e17b2659bb53441509c21a259f97979
chidubesteve <anikechidube@gmail.com>
2024-04-20 23:25:18 +0100

    Added timeout to close alert after 3 secs if user doesn't. added max-width and initial width properties for alert

--- client/src/components/Alert/Alert.jsx
+++ client/src/components/Alert/Alert.jsx
@@ -1,25 +1,36 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 
 // internal imports
-import style from './Alert.module.css'
+import style from "./Alert.module.css";
 
-const Alert = ({type, message}) => {
+const Alert = ({ type, message }) => {
   const [showAlert, setShowAlert] = useState(true);
 
-  const handleClick =(e) => {
+  // function to hide alert after 3 secs if user doesn't close it
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [showAlert]);
+
+  const handleClick = (e) => {
     e.preventDefault();
     setShowAlert(false);
-  }
+  };
   return (
-    (showAlert &&   
-    <div className={`${style.alert} ${style[type]}`}>
-      <span className={style.closebtn} onClick={handleClick}>
-        &times;
-      </span>
-      {message}
-    </div>
+    showAlert && (
+      <div className={`${style.alert} ${style[type]}`}>
+        <span className={style.closebtn} onClick={handleClick}>
+          &times;
+        </span>
+        {message}
+      </div>
     )
   );
-}
+};
 
-export default Alert  
+export default Alert;
