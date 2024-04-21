00ca76e7bac6184724addd8240849310076643af
chidubesteve <anikechidube@gmail.com>
2024-04-21 20:35:07 +0100

    Added custom Alert component and proper response catching from the server

--- client/src/components/HeroSection/FileUpload/FileUpload.jsx
+++ client/src/components/HeroSection/FileUpload/FileUpload.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useContext } from "react";
 import { Tooltip } from "react-tooltip";
-import axios from "axios";
 import { LuUploadCloud } from "react-icons/lu";
 
 // Internal imports
@@ -16,10 +15,8 @@ const FileUpload = ({ account, contract }) => {
   const [cid, setCid] = useState();
   const [showUploadButton, setShowUploadButton] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
-  const [fileLimit, setFileLimit] = useState(false);
+  const [alert, setAlert] = useState(null);
 
-
-  let limitExceeded = false;
   const readFile = (file) => {
     const reader = new window.FileReader();
     reader.readAsArrayBuffer(file);
@@ -28,6 +25,12 @@ const FileUpload = ({ account, contract }) => {
     };
     reader.onerror = () => {
       console.error("An error occurred when reading", file.name);
+      setAlert(
+        <Alert
+          type="error"
+          message={`An error occurred when reading ${file.name}. Kindly try again`}
+        />
+      );
     };
   };
 
@@ -44,7 +47,13 @@ const FileUpload = ({ account, contract }) => {
         console.log(droppedFile.name);
         console.log(droppedFile);
       } else if (droppedFiles.length > 1) {
-        alert("Drag and drop supports only single file");
+        setAlert(
+          <Alert
+            type="warning"
+            message={"Drag and drop supports only single file"}
+          />
+        );
+        return;
       }
     }
   };
@@ -68,10 +77,12 @@ const FileUpload = ({ account, contract }) => {
   };
 
   const handleClick = (e) => {
+    if (!showUploadButton) {
     // Make sure the target is not the upload button or its children
     if (!e.target.closest(`.${style.upload_button}`)) {
       inputRef.current.click();
     }
+  }
   };
 
   const handleFileSelect = (event) => {
@@ -91,91 +102,106 @@ const FileUpload = ({ account, contract }) => {
     e.preventDefault();
     if (file) {
       console.log(file);
-      try {
         const formData = new FormData();
         formData.append("file", file);
         // Send formData to backend server
-        await fetch("http://localhost:3001/upload", {
+        await fetch("/api/upload", {
           method: "POST",
           body: formData,
         })
           .then((response) => {
-            console.log("Response status:", response.status);
-            console.log("Response headers:", response.headers);
-            return response.json();
+            if (!response.ok) {
+              return response.json().then(data => {
+                throw new Error(data.message || "Oops, something went wrong! Kindly try again")
+              });
+            }
+            return response.json()
           })
           .then((data) => {
             console.log("Parsed JSON data:", data);
             console.log("server result gotten");
             const imgHash = `https://gateway.pinata.cloud/ipfs/${data.IpfsHash}`;
+            setAlert(
+              <Alert message={"File uploaded successfully!"} type={"success"} />
+            );
             console.log(imgHash);
+          })
+          .catch((err) => {
+            err.message === "Invalid file type!"
+              ? (setAlert(
+                  <Alert
+                    message={
+                      err.message +
+                      " Supported file types are images, .pdf, .docx, .epub, .txt files"
+                    }
+                    type={"warning"}
+                  />
+                ))
+              : (setAlert(<Alert message={err.message} type={"error"} />));
           });
-      } catch (error) {
-        console.log(error);
-        alert(error.message || "Oops an error occurred. kindly try again");
-      }
     }
   };
 
   return (
     <>
-    <Alert message={"Successfully uploaded file"} type={"success"} />
-    <div
-      className={`${style.file_upload_div} ${
-        theme === "light-mode" ? style.lightMode : ""
-      } dropzone ${isDragging ? style.dragging : ""}`}
-      id="file_upload_div"
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onClick={handleClick}
-      data-tooltip-id="my-tooltip"
-      data-tooltip-content={
-        "Select or drag-and-drop your files or images here."
-      }
-      data-tooltip-place="bottom"
-      data-tooltip-delay-show={400}
-      data-tooltip-delay-hide={400}
-    >
-      <Tooltip
-        id="my-tooltip"
-        className={`${style.toolTip} ${
+      {alert}
+      <div
+        className={`${style.file_upload_div} ${
           theme === "light-mode" ? style.lightMode : ""
-        }`}
-      />
-      <form action="/upload" encType="multipart/form-data">
-        <input
-          type="file"
-          ref={inputRef}
-          name="file"
-          id="select_file_input"
-          accept=".pdf, .txt, .docx, image/*, .epub"
-          onChange={handleFileSelect}
-          style={{ display: "none" }}
+        } dropzone ${isDragging ? style.dragging : ""}`}
+        id="file_upload_div"
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onClick={handleClick}
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content={
+          "Select or drag-and-drop your file or image here."
+        }
+        data-tooltip-place="bottom"
+        data-tooltip-delay-show={400}
+        data-tooltip-delay-hide={400}
+      >
+        <Tooltip
+          id="my-tooltip"
+          className={`${style.toolTip} ${
+            theme === "light-mode" ? style.lightMode : ""
+          }`}
         />
-      </form>
+        <form action="/upload" encType="multipart/form-data">
+          <input
+            type="file"
+            ref={inputRef}
+            name="file"
+            id="select_file_input"
+            accept=".pdf, .txt, .docx, image/*, .epub"
+            onChange={handleFileSelect}
+            style={{ display: "none" }}
+          />
+        </form>
 
-      {showUploadButton ? (
-        <div className={style.showUploadButtonContainer}>
-          <p>{fileName}</p>
-          <div>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                handleSubmit(e);
-              }}
-              className={style.upload_button}
-            >
-              Upload
-            </button>
+        {showUploadButton ? (
+          <div className={style.showUploadButtonContainer}>
+            <p>{fileName}</p>
+            <div>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  handleSubmit(e);
+                  setAlert(<Alert message={ "Uploading, please wait..."} type={'info'}/>)
+                }}
+                className={style.upload_button}
+              >
+                Upload File
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <LuUploadCloud className={style.upload_icon} />
-      )}
-    </div>
+        ) : (
+          <LuUploadCloud className={style.upload_icon} />
+        )}
+      </div>
     </>
   );
 };
