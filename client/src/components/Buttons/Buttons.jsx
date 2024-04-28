87872f580ff7d51b989bb9a239d986b2aa2798f3
chidubesteve <anikechidube@gmail.com>
2024-04-28 02:09:40 +0100

    Added disabled state and loading prop to Buttons component

--- client/src/components/Buttons/Buttons.jsx
+++ client/src/components/Buttons/Buttons.jsx
@@ -4,15 +4,16 @@ import React, { useContext } from "react";
 import style from "./Buttons.module.css";
 import { themeContext } from "../../Theme";
 
-const Buttons = ({ btnName, handleClick, className }) => {
+const Buttons = ({ btnName, handleClick, className, loading }) => {
   const {theme} = useContext(themeContext);
   return (
     <div className={ className ? className : style.box}>
       <button
         className={`${style.button} ${theme ==='light-mode' ? (style.lightMode) : ""}`}
         onClick={() => handleClick()}
+        disabled={loading}
       >
-        {btnName}
+        {loading ? "Loading..." : btnName}
       </button>
     </div>
   );
