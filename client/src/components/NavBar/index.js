60c7da56753f094cbc6bc6f71d0d586aab0bc3eb
chidubesteve <anikechidube@gmail.com>
2024-05-08 23:26:49 +0100

    Added RevokeModal component

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -2,3 +2,4 @@ export {default as ToggleButton} from './ToggleButton/ToggleButton'
 export {default as Buttons} from '../Buttons/Buttons'
 export { default as SideBar } from '../SideBar/SideBar'
 export { default as Modal } from "./Modal/Modal";
+export { default as RevokeModal} from './Modal/RevokeModal'
