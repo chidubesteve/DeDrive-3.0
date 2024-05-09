355d22b662113336c096bc56ad84e87ebe9db796
chidubesteve <anikechidube@gmail.com>
2024-05-09 14:18:28 +0100

    passed the setter state function for the modal and revokeModal component to open it from the sideBar

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,54 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { GrClose } from "react-icons/gr";
 
 // internal imports
 import { Buttons } from "../ComponentIndex";
 import style from "./SideBar.module.css";
 import { themeContext } from "../../Theme";
-import { Modal } from "../NavBar";
-import { RevokeModal } from "../NavBar";
 import { truncateAddress } from "../NavBar/NavBar";
 
 const SideBar = ({
   setOpenSideMenu,
-  contract,
   connectWallet,
   loadingConnectWallet,
   account,
+  setRevokeModal,
+  setModalOpen,
 }) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [revokeModal, setRevokeModal] = useState(false);
   const { theme } = useContext(themeContext);
 
   const closeSideBar = () => {
     setOpenSideMenu(false);
   };
   const openModal = () => {
-    if (!modalOpen) {
-      setModalOpen(true);
-      setRevokeModal(false);
-      setOpenSideMenu(false);
-    } else {
-      setModalOpen(false);
-    }
+    setModalOpen(true);
+    setRevokeModal(false);
+    setOpenSideMenu(false);
   };
   const openRevokeModal = () => {
-    if (!revokeModal) {
-      setRevokeModal(true);
-      setModalOpen(false);
-      setOpenSideMenu(false);
-    } else {
-      setRevokeModal(false);
-    }
+    setRevokeModal(true);
+    setModalOpen(false);
+    setOpenSideMenu(false);
   };
   const handleConnectClick = account.length > 0 ? () => {} : connectWallet;
 
   return (
     <>
-      {modalOpen && <Modal setModalOpen={setModalOpen} contract={contract} />}
-      {revokeModal && (
-        <RevokeModal setRevokeModal={setRevokeModal} contract={contract} />
-      )}
       <div
         className={`${style.sideBar} ${
           theme === "light-mode" ? style.lightMode : ""
@@ -87,31 +72,25 @@ const SideBar = ({
                   About me
                 </a>
               </li>
-              {account.length > 0
-                ? !revokeModal && (
-                    <li
-                      className={style.navBoxItem}
-                      onClick={() => openRevokeModal()}
-                    >
-                      Revoke
-                    </li>
-                  )
-                : ""}
+              {account.length > 0 && (
+                <li
+                  className={style.navBoxItem}
+                  onClick={() => openRevokeModal()}
+                >
+                  Revoke
+                </li>
+              )}
             </ul>
           </nav>
         </div>
         <div className={style.sideBar_button}>
-          {account.length > 0
-            ? !modalOpen && (
-                <Buttons
-                  btnName="Share"
-                  handleClick={() => {
-                    openModal();
-                  }}
-                  className={style.btn}
-                />
-              )
-            : ""}
+          {account.length > 0 && (
+            <Buttons
+              btnName="Share"
+              handleClick={() => openModal()}
+              className={style.btn}
+            />
+          )}
 
           {/* // Connect wallet button */}
           <Buttons
